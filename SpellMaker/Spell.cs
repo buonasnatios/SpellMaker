600cee69d6d8896eb91f6c710ad784cbf75bf242
Brandist <brandondisseldorp@hotmail.com>
2024-01-31 10:33:31 +0100

    Moving the sentence generation to SpellSentenceGenerator.cs

--- SpellMaker/Spell.cs
+++ SpellMaker/Spell.cs
@@ -9,10 +9,11 @@ public class Spell(List<IInvocation> invocations, string spellName)
     public Spell(string spellName) : this([], spellName)
     {
     }
+    
 
-    private List<IInvocation> Invocations { get; set; } = invocations;
+    public List<IInvocation> Invocations { get; set; } = invocations;
     public string SpellName { get; set; } = spellName;
-    public string SpellSentence => GetSpellSentence();
+    
     public float Size { get; set; } = 1f;
     public float Damage { get; set; } = 0.0f;
     public float Speed { get; set; } = 0.0f;
@@ -25,7 +26,8 @@ public class Spell(List<IInvocation> invocations, string spellName)
     public float Piercing  => (Weight+Speed)/2;
     public SpellShape SpellShape { get; set; }
     public ElementType? ElementType { get; set; }
-    private Target? Target { get; set; }
+    public Target? Target { get; set; }
+    public string SpellSentence => new SpellSentenceGenerator(this).GenerateSentence();
 
     public int AddInvocation(IInvocation? invocation)
     {
@@ -37,114 +39,7 @@ public class Spell(List<IInvocation> invocations, string spellName)
         ImplementAdditionEffects(invocation.Addition);
         return 0;
     }
-
-    private string GetSpellSentence()
-    {
-        var sentence = AggregateInvocations();
-        sentence = AddTarget(sentence);
-        sentence = AddSpellStats(sentence);
-
-        return sentence;
-    }
-
-    private string AddSpellStats(string sentence)
-    {
-        sentence = AddDamageText(sentence);
-        sentence = AddSizeText(sentence);
-        sentence = AddRangeText(sentence);
-        sentence = EndSentence(sentence);
-        return sentence;
-    }
-
-    private IEnumerable<IInvocation> GetOrderedInvocation()
-    {
-        List<IInvocation> invocations = [];
-        foreach (var invocation in Invocations)
-        {
-            if (invocation is { InvocationOrder.Count: > 0 })
-            {
-                invocations.Add(invocation);
-            }
-        }
-
-        return invocations;
-    }
-
-    private static string EndSentence(string sentence)
-    {
-        return sentence.Remove(sentence.Length - 1, 1) + ".";
-    }
-
-    private string AggregateInvocations()
-    {
-        return Invocations.Aggregate("", (current, invocation) =>
-        {
-            return current + invocation.InvocationType switch
-            {
-                InvocationType.Noun => $"{invocation.Name} ",
-                InvocationType.Verb => $"{invocation.Name} ",
-                InvocationType.Adjective => $"{invocation.Name} ",
-                InvocationType.Target => "",
-                InvocationType.Self => "",
-                _ => throw new ArgumentOutOfRangeException()
-            };
-        });
-    }
-
-    private string AddTarget(string sentence)
-    {
-        switch (Target)
-        {
-            case Data.Enums.Target.Self:
-                return sentence + "yourself ";
-            case Data.Enums.Target.Ally:
-                return sentence + "an ally ";
-            case Data.Enums.Target.Enemy:
-                return sentence + "an enemy ";
-            case Data.Enums.Target.NonSelf:
-                return sentence + "anyone excluding yourself ";
-            case Data.Enums.Target.Any:
-                return sentence + "anyone ";
-            case null:
-                break;
-            default:
-                throw new ArgumentOutOfRangeException();
-        }
-
-        return sentence;
-    }
-
-    private string AddDamageText(string sentence)
-    {
-        return Damage switch
-        {
-            0 => sentence,
-            > 0 => sentence + $"dealing {Damage} damage ",
-            < 0 => sentence + $"healing {Damage * -1} health ",
-            _ => throw new ArgumentOutOfRangeException()
-        };
-    }
-
-    private string AddSizeText(string sentence)
-    {
-        return Size switch
-        {
-            0 => sentence,
-            > 0 => sentence + $"with a radius of {Size} meters ",
-            _ => throw new ArgumentOutOfRangeException()
-        };
-    }
-
-    private string AddRangeText(string sentence)
-    {
-        return Range switch
-        {
-            0 => sentence,
-            > 0 => sentence + $"and a range of {Range} meters ",
-            _ => throw new ArgumentOutOfRangeException()
-        };
-    }
-
+    
     private void ImplementAdditionEffects(object addition)
     {
         switch (addition)
